d3232f9344cb7a70dfbde0c54d927c03c4ce9ed2
Angel Peña <angel.pena@s4e.cl>
2021-06-20 11:25:28 -0400

    heroku procfile

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
   res.send("Hola Mundo!");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Gello Habits sample endpoint, listening port 3000");
 });
