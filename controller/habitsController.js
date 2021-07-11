cfbb5ac3877f197a047c308cf3db936fdf32961f
Nicolas Cardenas <nicolas.cardenas722@gmail.com>
2021-07-11 15:25:36 -0500

    eslinter applied to controller

--- controller/habitsController.js
+++ controller/habitsController.js
@@ -1,80 +1,77 @@
 const habitsService = require('../service/habitsService');
-const habitLogService = require("../service/habitLogService");
+const habitLogService = require('../service/habitLogService');
 
 exports.getAllHabits = (req, res) => {
-  //Map the user of the auth in the header
+  // Map the user of the auth in the header
   habitsService.fetchAllHabits()
     .then((data) => {
       res.status(200).send(data);
     }).catch((err) => {
       res.status(err.status || 500).send({ name: err.name, message: err.message });
-    })
+    });
 };
 
 exports.getHabit = (req, res) => {
-  const { id } = req.params
+  const { id } = req.params;
   habitsService.fetchHabit(id)
     .then((data) => {
       res.status(200).send(data);
     }).catch((err) => {
       res.status(err.status || 500).send({ name: err.name, message: err.message });
-    })
+    });
 };
 
 exports.deleteHabit = (req, res) => {
-  const { id } = req.params
+  const { id } = req.params;
   habitsService.deleteHabit(id)
     .then(() => {
       res.status(200).send(null);
     }).catch((err) => {
       res.status(err.status || 500).send({ name: err.name, message: err.message });
-    })
+    });
 };
 
 exports.updateHabit = (req, res) => {
-  const { id } = req.params
-  const habitData = req.body
+  const { id } = req.params;
+  const habitData = req.body;
   habitsService.updateHabit(habitData, id)
     .then((data) => {
       res.status(200).send(data);
     }).catch((err) => {
       res.status(err.status || 500).send({ name: err.name, message: err.message });
-    })
+    });
 };
 
 exports.createHabit = (req, res) => {
-  const habitData = req.body
+  const habitData = req.body;
   habitsService.createHabit(habitData)
     .then((data) => {
       res.status(201).send(data);
     }).catch((err) => {
       res.status(err.status || 500).send({ name: err.name, message: err.message });
-    })
+    });
 };
 
 exports.getAllHabitLogs = (req, res) => {
-  const logs = habitLogService
+  habitLogService
     .fetchHabitLogs()
     .then((data) => {
       if (data) res.status(200).send(data);
       else res.status(404);
     })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send({ message: "Paso un peo" });
+    .catch(() => {
+      res.status(500).send({ message: 'Paso un peo' });
     });
 };
 
 exports.registerHabitLog = (req, res) => {
   const data = req.body;
-  const logs = habitLogService
+  habitLogService
     .createHabitLog({ data })
-    .then((data) => {
-      res.status(201).send(data);
+    .then((result) => {
+      res.status(201).send(result);
     })
-    .catch((err) => {
-      console.log(err);
-
-      res.status(500).send({ message: "Paso un peo" });
+    .catch(() => {
+      res.status(500).send({ message: 'Paso un peo' });
     });
 };
--- controller/userController.js
+++ controller/userController.js
@@ -1,30 +1,26 @@
-userService = require("../service/userService");
+const userService = require('../service/userService');
 
 exports.getUserData = (req, res) => {
-  const id = req.params.id;
-  const user = userService
+  const { id } = req.params;
+  userService
     .fetchUser({ id })
     .then((data) => {
       if (data) res.status(200).send(data);
       else res.status(404);
     })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send({ message: "Paso un peo" });
-    });
+    .catch(() => res.status(500).send({ message: 'Paso un peo' }));
 };
 
 exports.updateUserData = (req, res) => {
-  const id = req.params.id;
+  const { id } = req.params;
   const data = req.body;
-  const user = userService
+  userService
     .updateUser({ id, data })
-    .then((data) => {
-      if (data) res.status(200).send(data);
+    .then((result) => {
+      if (result) res.status(200).send(result);
       else res.status(404);
     })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send({ message: "Paso un peo" });
+    .catch(() => {
+      res.status(500).send({ message: 'Paso un peo' });
     });
 };
