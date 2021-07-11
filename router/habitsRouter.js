4fcad74f23822fa86199cc74eaef485e6141c7f0
Nicolas Cardenas <nicolas.cardenas722@gmail.com>
2021-07-11 15:13:03 -0500

    eslint applied at router folder

--- router/habitsRouter.js
+++ router/habitsRouter.js
@@ -1,21 +1,21 @@
-const express = require("express");
+const express = require('express');
 
-const controller = require("../controller/habitsController");
+const controller = require('../controller/habitsController');
 
 const router = express.Router();
 
-router.get("/habits", controller.getAllHabits);
+router.get('/habits', controller.getAllHabits);
 
-router.get("/habits/:id", controller.getHabit);
+router.get('/habits/:id', controller.getHabit);
 
-router.delete("/habits/:id", controller.deleteHabit);
+router.delete('/habits/:id', controller.deleteHabit);
 
-router.put("/habits/:id", controller.updateHabit);
+router.put('/habits/:id', controller.updateHabit);
 
-router.post("/habits", controller.createHabit);
+router.post('/habits', controller.createHabit);
 
-router.post("/logs", controller.registerHabitLog);
+router.post('/logs', controller.registerHabitLog);
 
-router.get("/logs", controller.getAllHabitLogs);
+router.get('/logs', controller.getAllHabitLogs);
 
 module.exports = router;
