d7583e69ae97d4a7f13433eeb4f71e2265127ff1
Nicolas Cardenas <nicolas.cardenas722@gmail.com>
2021-07-11 15:12:05 -0500

    eslint applied in services folder

--- service/userService.js
+++ service/userService.js
@@ -1,11 +1,5 @@
-const userDAO = require("../repository/userDAO");
+const userDAO = require('../repository/userDAO');
 
-exports.fetchUser = ({ user, id }) => {
-  console.log(id);
-  return userDAO.findById(id);
-};
+exports.fetchUser = ({ id }) => userDAO.findById(id);
 
-exports.updateUser = ({ user, id, data }) => {
-  console.log(data);
-  return userDAO.updateUser({ id, data });
-};
+exports.updateUser = ({ id, data }) => userDAO.updateUser({ id, data });
